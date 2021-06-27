6cf60c02da4c0dc3dff1a467124961f65f00cc08
hughniteddotcom <hughnited@gmail.com>
2021-06-27 14:31:07 +0200

    .

--- BookStore.Domain/ViewModels/SubscriptionViewModel.cs
+++ BookStore.Domain/ViewModels/SubscriptionViewModel.cs
@@ -2,19 +2,13 @@
 {
     public class SubscriptionViewModel
     {
-        public SubscriptionViewModel()
-        {
-            Book = new BookViewModel();
-            User = new UserViewModel();
-        }
-
         public int BookId { get; set; }
 
         public int UserId { get; set; }
 
-        public BookViewModel Book { get; set; }
+        public BookViewModel Book { get; set; } = new BookViewModel();
 
-        public UserViewModel User { get; set; }
+        public UserViewModel User { get; set; } = new UserViewModel();
 
     }
 }
